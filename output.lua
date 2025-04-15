37385df0d938f511167a39a59ad699ff412c8a7c
BoredScripter <BoredScripterRBX@gmail.com>
2025-04-15 10:22:18 +0200

    Added anti afk

--- output.lua
+++ output.lua
@@ -299,7 +299,14 @@ end
 end
 
 -- Entry Point
-
+-- Init anti afk
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+print("Anti afk on for pc")
 
 -- modules
 local Globals = __require("model.Globals")
