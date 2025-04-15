57d9e779d3b525b03cf965aa2e170cd845e5cd16
BoredScripter <BoredScripterRBX@gmail.com>
2025-04-15 22:37:57 +0200

    Added task system and new sell system so it teleports to sell area maybe no ban anymore?

--- main.lua
+++ main.lua
@@ -1,15 +1,19 @@
+local Players = game:GetService("Players")
+while not Players.LocalPlayer do
+    task.wait()
+end
+
 -- Init anti afk
 local vu = game:GetService("VirtualUser")
-game:GetService("Players").LocalPlayer.Idled:connect(function()
-   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-   wait(1)
-   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+Players.LocalPlayer.Idled:Connect(function()
+    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
 end)
 print("Anti afk on for pc")
 
 -- modules
 local Globals = __require("model.Globals")
 local View = __require("view.View")
-
 
 View()
